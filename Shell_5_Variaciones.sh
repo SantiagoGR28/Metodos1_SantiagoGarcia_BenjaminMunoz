46c6d91e24df980bfec8b313a5e8e46ec8be6da1
BenjaminMunozHurtado <88908950+BenjaminMunozHurtado@users.noreply.github.com>
2022-08-23 19:44:59 -0500

    Create Shell_5_Variaciones.sh

---
+++ Shell_5_Variaciones.sh
@@ -0,0 +1,24 @@
+
+n=20
+r=3
+resta=n-r
+factorialnumerador=1
+factorialdenominador=1
+
+while [ $n -gt 1 ]
+	do
+		let factorialnumerador=$factorialnumerador*$n
+		let n=$n-1
+	done
+
+while [ $(resta) -gt 1 ]
+	do
+		let factorialdenominador=$factorialdenominador*$resta
+		let resta=$resta-1
+	done
+  
+ variaciones=$(($factorialnumerador/$factorialdenominador))
+ 
+ echo $variaciones
+
+#la complejidad es de O(2n-r)
