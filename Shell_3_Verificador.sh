95d598cb2387d37f6d41f2bd0ced4a8bad38b8c0
BenjaminMunozHurtado <88908950+BenjaminMunozHurtado@users.noreply.github.com>
2022-08-23 19:36:14 -0500

    Create Shell_3_Verificador.sh

---
+++ Shell_3_Verificador.sh
@@ -0,0 +1,15 @@
+pass=0
+function checkvalue(){
+	value=$1
+	if [ vale -eq 0 ] || [ value -eq 1 ]; then
+  		pass=1
+  	else
+  		echo "vuelve a intentar"
+  fi
+}
+
+while [ $pass -eq 0 ]
+do
+	read valor
+	checkvalue valor
+done
